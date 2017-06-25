051fa219c8cd00d5b736fe9a4c9c1ab7f7c8044c
logicalguess <logicalguess@gmail.com>
2017-06-24 22:54:01 -0400

    ask

--- src/main/java/logicalguess/test/QueryRun.java
+++ src/main/java/logicalguess/test/QueryRun.java
@@ -9,18 +9,26 @@ import logicalguess.persistence.query.ItemsQueryProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+
+import static akka.pattern.PatternsCS.ask;
+
 /**
  * Created by logicalguess on 6/24/17.
  */
 public class QueryRun {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         final Logger log = LoggerFactory.getLogger(QueryRun.class);
         log.info("Start actor system");
         final ActorSystem actorSystem = ActorSystem.create("items");
 
         ActorRef items = actorSystem.actorOf(ItemsQueryProcessor.props("1"), "items");
 
-        items.tell(ItemsGet.create("1"), null);
+        //items.tell(ItemsGet.create("1"), null);
+        CompletionStage<Object> f = ask(items, ItemsGet.create("1"), 3000L);
+        Thread.sleep(3000);
+        System.out.println(f.toCompletableFuture().get());
 
     }
 }
